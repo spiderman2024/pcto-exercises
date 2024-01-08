cc36de19e50561cf7d3116097f74ce360b896424
spiderman2024 <155971328+spiderman2024@users.noreply.github.com>
2024-01-08 15:13:47 +0100

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float g = 7; //flag ; %f
+    char l = 'L' ; //flag ; %c
+    char m = 'M' ; //flag ; %c
+    int a = 81; //flag ; %d
+    printf("chiave : %f %c %c %d", g, l, m, a);
+
+    return 0;
+
+}
